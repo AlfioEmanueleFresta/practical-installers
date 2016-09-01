9eab6eb22684c1b26037e9eefa0ed8b5f4e493c7
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-09-01 16:52:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Practicals Installer
+
+This is a script that can be used to download and install many
+practicals on a Cyber Practicals base VM.
+
+
+## Available practicals
+
+| ID  | Practical Name             | Author          |
+|-----|----------------------------|-----------------|
+| xss | Input Sanitisation and XSS | Alfio E. Fresta |
+| otp | One-Time Pad               | Alfio E. Fresta |
+| ecb | Information vs. Data       | Alfio E. Fresta |
+
+
+## Usage
+
+Login as root
+```
+./install <practical-id>
+```
+
+Where `pratical-id` is one of the IDs from the table above.
---
+++ practicals/lib/files.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import subprocess
+
+
+def run_cmd(command, verbose=True, return_output=False):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    output = ''
+    for line in proc.stdout:
+        line = line.decode('ascii')
+        output += line
+        if verbose:
+            sys.stdout.write(line)
+            sys.stdout.flush()
+    out, err = proc.communicate()
+    if err:
+        sys.stdout.write(err.decode('ascii'))
+    if return_output:
+        return output
+
+
+def file_copy(src_filename, dst_filename):
+    run_cmd(["cp", src_filename, dst_filename])
+
+
+def file_write(filename, contents):
+    with open(filename, "w") as f:
+        f.write(contents)
+
+
+def file_append(filename, contents):
+    with open(filename, "a") as f:
+        f.write(contents)
+
+
+def file_read(filename):
+    with open(filename, "r") as f:
+        x = f.read()
+    return x
+
+
+def file_delete(filename):
+    run_cmd(["rm", filename])
+
+
+def get_tmp_dir():
+    return run_cmd(['mktemp', '-d'], return_output=True).strip()
+
+
+def get_current_user():
+    return run_cmd(['whoami'], return_output=True).strip()
